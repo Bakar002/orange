eafdd5e0f32c315c8903778534c13d22a3418dfa
Bakar002 <abubakaramjad78600@gmail.com>
2024-08-05 17:03:44 +0500

    adding

--- orange/src/components/Header.js
+++ orange/src/components/Header.js
@@ -2,8 +2,9 @@ import React from "react";
 
 import digilligent from "../icons/Digilligent.svg";
 import whatsapp from "../icons/whatsapp.svg";
-
+import "./Header.css"
 function Header() {
+  
   return (
     <div className="flex justify-between items-center pt-2 px-4 md:px-[83px]">
       <div className="flex items-center">
@@ -11,12 +12,10 @@ function Header() {
       </div>
       <div className="flex space-x-4">
         
-
-      <a href="#quoteForm" className="bg-orange-500 text-white py-2 px-2 rounded-md font-clash md:py-3 md:px-8 lg:py-2 lg:px-10">
-
+      <a href="#quoteForm" class="button">
   Get a Quote
-
 </a>
+
 
         <button className="rounded-[6px] bg-[#5BF877] w-10 h-10 flex items-center justify-center">
           <img src={whatsapp} alt="WhatsApp Logo" />
