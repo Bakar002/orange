0d4c2f2cb09ea1d7ea465c05dcd8cd2ad1e68b8d
Bakar002 <abubakaramjad78600@gmail.com>
2024-08-17 14:08:19 +0500

    add

--- orange/src/components/Carousel.js
+++ orange/src/components/Carousel.js
@@ -68,7 +68,7 @@ function ProjectCarousel() {
         Take a look at the{" "}
         <span className="text-orange-500">latest projects</span> we’ve done.
       </h2>
-      <div className="mx-auto w-full  max-w-7xl mb-0 sm:mb-[113px] overflow-visible ">
+      <div className=" mx-auto w-full  max-w-7xl mb-0 sm:mb-[113px] overflow-visible ">
         <Carousel
           responsive={responsive}
           infinite={true}
@@ -84,29 +84,30 @@ function ProjectCarousel() {
           draggable={true}
         >
         {projects.map((project, index) => (
-          <div
-            key={index}
-            className="rounded-2xl relative px-6 transform transition-transform duration-300 hover:-rotate-45 hover:z-10"
-            style={{ overflow: 'visible' }}
-          >
-            <img
-              src={project.image}
-              alt={project.name}
-              className=" rounded-2xl w-[375px] h-[320px] object-contain shadow-lg p-10 "
-              style={{ borderRadius: '2rem', transformOrigin: 'center center' }}
-              />
-            <div
-              className="absolute bottom-0 left-0 w-full p-4 rounded-b-lg text-white text-left"
-              style={{
-                background:
-                  "linear-gradient(to top, rgba(0, 0, 10, 0.7), transparent)",
-              }}
-            >
-              <h3 className="text-xl font-semibold pl-8 font-aeonik">{project.name}</h3>
-              <p className="text-sm pl-8 font-aeonik">{project.detail}</p>
-            </div>
-          </div>
-        ))}
+  <div
+    key={index}
+    className="relative rounded-2xl px-6 transform transition-transform duration-300 hover:-rotate-45 hover:z-10"
+    style={{ overflow: 'visible' }}
+  >
+    <img
+      src={project.image}
+      alt={project.name}
+      className="rounded-2xl w-full sm:w-[375px] h-[220px] sm:h-[320px] object-cover shadow-lg"
+      style={{ borderRadius: '2rem', transformOrigin: 'center center' }}
+    />
+    <div
+      className="absolute bottom-0 left-0 w-full p-4 text-white text-left rounded-b-lg"
+      style={{
+        background:
+          "linear-gradient(to top, rgba(0, 0, 10, 0.7), transparent)",
+        borderRadius: '0 0 2rem 2rem',
+      }}
+    >
+      <h3 className="text-lg sm:text-xl font-semibold pl-4 sm:pl-8 font-aeonik">{project.name}</h3>
+      <p className="text-xs sm:text-sm pl-4 sm:pl-8 font-aeonik">{project.detail}</p>
+    </div>
+  </div>
+))}
 
         </Carousel>
       </div>
